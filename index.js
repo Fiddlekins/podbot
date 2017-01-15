0640e0ca433d0f033e11307d1a81613ec5ffe4f5
Fiddlekins <fiddlekins@gmail.com>
2017-01-15 16:24:45 +0000

    Podbot will now join and leave a voiceChannel when commanded, and will manage to record everything people say whilst in it.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const Discord = require('discord.js');
 
 const TOKEN = fs.readFileSync('./token', 'utf8').trim(); // Trim because linux
@@ -10,40 +11,98 @@ class Podbot {
 	constructor(token){
 		this.client = new Discord.Client();
 		this.commandCharacter = '/';
+		this.podcastsPath = Podbot._makePodcastsDirectory();
 
-		this.client.on('ready', () =>{
-			console.log('Ready!');
-		});
+		this._voiceConnections = new Map();
+		this._voiceReceivers = new Map();
+		this._podcastNames = new Map();
+
+		this.client.on('ready', this._onReady.bind(this));
 
 		this.client.on('message', this._onMessage.bind(this));
 
-		this.client.on('voiceStateUpdate', (oldUser, newUser) =>{
-			if (newUser.user.equals(this.client.user)) {
-				return;
-			}
-			if (newUser.voiceChannel) {
-				newUser.voiceChannel.join().then((voiceConnection) =>{
-					let voiceReceiver = voiceConnection.createReceiver();
-					let inputStream = voiceReceiver.createPCMStream(newUser);
-					let outputStream = fs.createWriteStream('./voice');
-					inputStream.pipe(outputStream);
-				}).catch(console.error);
-			}
-		});
+		this.client.on('guildMemberSpeaking', this._onGuildMemberSpeaking.bind(this));
 
 		this.client.login(token).catch(console.error);
+	}
+
+	_onReady(){
+		console.log('Ready!');
 	}
 
 	_onMessage(message){
 		if (message.content.charAt(0) === this.commandCharacter) {
 			switch (message.content.slice(1)) {
 				case 'podon':
+					this._podon(message.member);
 					break;
 				case 'podoff':
+					this._podoff(message.member);
 					break;
 			}
 		}
 	}
+
+	_onGuildMemberSpeaking(member, speaking){
+		if (speaking && member.voiceChannel) {
+			let receiver = this._voiceReceivers.get(member.voiceChannelID);
+			if (receiver) {
+				let podcastName = this._podcastNames.get(member.voiceChannelID);
+				let outputPath = path.join(this.podcastsPath, podcastName, `${member.id}-${Date.now()}`);
+				let inputStream = receiver.createPCMStream(member);
+				let outputStream = fs.createWriteStream(outputPath);
+				inputStream.pipe(outputStream);
+			}
+		}
+	}
+
+	_podon(member){
+		if (!this._checkMemberHasPermissions(member)) {
+			return;
+		}
+		if (!member.voiceChannel) {
+			return;
+		}
+
+		let podcastName = `${member.voiceChannelID}-${Date.now()}`;
+		this._podcastNames.set(member.voiceChannelID, podcastName);
+		Podbot._makeDirectory(path.join(this.podcastsPath, podcastName));
+
+		member.voiceChannel.join().then((voiceConnection) =>{
+			this._voiceConnections.set(member.voiceChannelID, voiceConnection);
+			this._voiceReceivers.set(member.voiceChannelID, voiceConnection.createReceiver());
+		}).catch(console.error);
+	}
+
+	_podoff(member){
+		if (!this._checkMemberHasPermissions(member)) {
+			return;
+		}
+
+		this._voiceReceivers.get(member.voiceChannelID).destroy();
+		this._voiceReceivers.delete(member.voiceChannelID);
+		this._voiceConnections.get(member.voiceChannelID).disconnect();
+		this._voiceConnections.delete(member.voiceChannelID);
+		this._podcastNames.delete(member.voiceChannelID);
+	}
+
+	_checkMemberHasPermissions(member){
+		return true;
+	}
+
+	static _makePodcastsDirectory(){
+		let dir = path.join('.', 'podcasts');
+		Podbot._makeDirectory(dir);
+		return dir;
+	}
+
+	static _makeDirectory(dir){
+		try {
+			fs.mkdirSync(dir);
+		} catch (err) {
+			// Don't care, presumably the folder exists already
+		}
+	}
 }
 
-const fidbot = new Podbot(TOKEN);
+const podbot = new Podbot(TOKEN);
