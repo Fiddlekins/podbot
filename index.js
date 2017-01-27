d7d16795db0d1aed27248bed292ba04cf7ba4fd2
Fiddlekins <fiddlekins@gmail.com>
2017-01-27 21:08:32 +0000

    Got some code that tries to convert opus streams to PCM on the fly, and some code that tries to capture all the opus packets rather than using the createOpusStream() on the offchance that is why my opus is unplayable.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const fs = require('fs');
 const path = require('path');
+const child_process = require('child_process');
+const StringDecoder = require('string_decoder').StringDecoder;
 const Discord = require('discord.js');
 
-const TOKEN = fs.readFileSync('./token', 'utf8').trim(); // Trim because linux
 
+const TOKEN = fs.readFileSync('./token', 'utf8').trim(); // Trim because linux
 
 class Podbot {
 	constructor(token){
@@ -17,11 +19,13 @@ class Podbot {
 		this._voiceReceivers = new Map();
 		this._podcastNames = new Map();
 
+		this._writeStreams = new Map();
+
 		this.client.on('ready', this._onReady.bind(this));
 
 		this.client.on('message', this._onMessage.bind(this));
 
-		this.client.on('guildMemberSpeaking', this._onGuildMemberSpeaking.bind(this));
+		// this.client.on('guildMemberSpeaking', this._onGuildMemberSpeaking.bind(this));
 
 		this.client.login(token).catch(console.error);
 	}
@@ -49,9 +53,20 @@ class Podbot {
 			if (receiver) {
 				let podcastName = this._podcastNames.get(member.voiceChannelID);
 				let outputPath = path.join(this.podcastsPath, podcastName, `${member.id}-${Date.now()}`);
-				let inputStream = receiver.createPCMStream(member);
+				let inputStream = receiver.createOpusStream(member);
 				let outputStream = fs.createWriteStream(outputPath);
-				inputStream.pipe(outputStream);
+				// inputStream.pipe(outputStream);
+
+				// let ffmpegArgs = '-f s32le -ar 24k -ac 2 -i pipe:0 -ar 48000 -ac 2 -acodec libopus -ab 256k -f s32le pipe:1'.split(' ');
+				let ffmpegArgs = '-acodec libopus -f s32le -ar 24k -ac 2 -i pipe:0 -f s32le -ar 24k -ac 2 pipe:1'.split(' ');
+
+				let ffmpeg = child_process.spawn('ffmpeg', ffmpegArgs, {shell: true});
+				inputStream.pipe(ffmpeg.stdin);
+				ffmpeg.stdout.pipe(outputStream);
+				let decoder = new StringDecoder('utf8');
+				ffmpeg.stderr.on('data', (data) =>{
+					console.log(decoder.write(data));
+				});
 			}
 		}
 	}
@@ -70,7 +85,28 @@ class Podbot {
 
 		member.voiceChannel.join().then((voiceConnection) =>{
 			this._voiceConnections.set(member.voiceChannelID, voiceConnection);
-			this._voiceReceivers.set(member.voiceChannelID, voiceConnection.createReceiver());
+			// actually just add an event to listen to dat opus
+			let voiceReceiver = voiceConnection.createReceiver();
+			voiceReceiver.on('opus', (user, data) =>{
+				console.log(data);
+				let writeStream = this._writeStreams.get(user.id);
+				if (!writeStream) {
+					let outputPath = path.join(this.podcastsPath, podcastName, `${user.id}-${Date.now()}`);
+					writeStream = fs.createWriteStream(outputPath);
+					writeStream.on('finish', () =>{
+						console.log('finish');
+						this._writeStreams.delete(user.id);
+					});
+					writeStream.on('close', () =>{
+						console.log('close');
+						this._writeStreams.delete(user.id);
+					});
+					this._writeStreams.set(user.id, writeStream);
+				}
+				writeStream.write(data);
+			});
+			// insert ended
+			this._voiceReceivers.set(member.voiceChannelID, voiceReceiver);
 		}).catch(console.error);
 	}
 
