f0a1a00663448653aabf0b68f85190c9b03a3ebc
Fiddlekins <fiddlekins@gmail.com>
2017-02-17 22:59:50 +0000

    Switched to listening to the opus event to capture all the packets and converting them into comma separated hex strings in order to create an opus format that is later readable

--- index.js
+++ index.js
@@ -2,39 +2,35 @@
 
 const fs = require('fs');
 const path = require('path');
-const child_process = require('child_process');
-const StringDecoder = require('string_decoder').StringDecoder;
 const Discord = require('discord.js');
 
 
 const TOKEN = fs.readFileSync('./token', 'utf8').trim(); // Trim because linux
 
 class Podbot {
-	constructor(token){
+	constructor(token) {
 		this.client = new Discord.Client();
 		this.commandCharacter = '/';
 		this.podcastsPath = Podbot._makePodcastsDirectory();
 
 		this._voiceConnections = new Map();
 		this._voiceReceivers = new Map();
-		this._podcastNames = new Map();
-
 		this._writeStreams = new Map();
 
 		this.client.on('ready', this._onReady.bind(this));
 
 		this.client.on('message', this._onMessage.bind(this));
 
-		// this.client.on('guildMemberSpeaking', this._onGuildMemberSpeaking.bind(this));
+		this.client.on('guildMemberSpeaking', this._onGuildMemberSpeaking.bind(this));
 
 		this.client.login(token).catch(console.error);
 	}
 
-	_onReady(){
+	_onReady() {
 		console.log('Ready!');
 	}
 
-	_onMessage(message){
+	_onMessage(message) {
 		if (message.content.charAt(0) === this.commandCharacter) {
 			switch (message.content.slice(1)) {
 				case 'podon':
@@ -47,31 +43,25 @@ class Podbot {
 		}
 	}
 
-	_onGuildMemberSpeaking(member, speaking){
-		if (speaking && member.voiceChannel) {
+	_onGuildMemberSpeaking(member, speaking) {
+		// Close the writeStream when a member stops speaking
+		if (!speaking && member.voiceChannel) {
 			let receiver = this._voiceReceivers.get(member.voiceChannelID);
 			if (receiver) {
-				let podcastName = this._podcastNames.get(member.voiceChannelID);
-				let outputPath = path.join(this.podcastsPath, podcastName, `${member.id}-${Date.now()}`);
-				let inputStream = receiver.createOpusStream(member);
-				let outputStream = fs.createWriteStream(outputPath);
-				// inputStream.pipe(outputStream);
-
-				// let ffmpegArgs = '-f s32le -ar 24k -ac 2 -i pipe:0 -ar 48000 -ac 2 -acodec libopus -ab 256k -f s32le pipe:1'.split(' ');
-				let ffmpegArgs = '-acodec libopus -f s32le -ar 24k -ac 2 -i pipe:0 -f s32le -ar 24k -ac 2 pipe:1'.split(' ');
-
-				let ffmpeg = child_process.spawn('ffmpeg', ffmpegArgs, {shell: true});
-				inputStream.pipe(ffmpeg.stdin);
-				ffmpeg.stdout.pipe(outputStream);
-				let decoder = new StringDecoder('utf8');
-				ffmpeg.stderr.on('data', (data) =>{
-					console.log(decoder.write(data));
-				});
+				let writeStream = this._writeStreams.get(member.id);
+				if (writeStream) {
+					this._writeStreams.delete(member.id);
+					writeStream.end(err => {
+						if (err) {
+							console.error(err);
+						}
+					});
+				}
 			}
 		}
 	}
 
-	_podon(member){
+	_podon(member) {
 		if (!this._checkMemberHasPermissions(member)) {
 			return;
 		}
@@ -80,37 +70,38 @@ class Podbot {
 		}
 
 		let podcastName = `${member.voiceChannelID}-${Date.now()}`;
-		this._podcastNames.set(member.voiceChannelID, podcastName);
 		Podbot._makeDirectory(path.join(this.podcastsPath, podcastName));
 
-		member.voiceChannel.join().then((voiceConnection) =>{
+		member.voiceChannel.join().then((voiceConnection) => {
 			this._voiceConnections.set(member.voiceChannelID, voiceConnection);
-			// actually just add an event to listen to dat opus
 			let voiceReceiver = voiceConnection.createReceiver();
-			voiceReceiver.on('opus', (user, data) =>{
-				console.log(data);
+			voiceReceiver.on('opus', (user, data) => {
+				let hexString = data.toString('hex');
 				let writeStream = this._writeStreams.get(user.id);
 				if (!writeStream) {
-					let outputPath = path.join(this.podcastsPath, podcastName, `${user.id}-${Date.now()}`);
+					/* If there isn't an ongoing writeStream and a frame of silence is received then it must be the
+					 *   left over trailing silence frames used to signal the end of the transmission.
+					 * If we do not ignore this frame at this point we will create a new writeStream that is labelled
+					 *   as starting at the current time, but there will actually be a time delay before it is further
+					 *   populated by data once the user has begun speaking again.
+					 * This delay would not be captured however since no data is sent for it, so the result would be
+					 *   the audio fragments being out of time when reassembled.
+					 * For this reason a packet of silence cannot be used to create a new writeStream.
+					 */
+					if (hexString === 'f8fffe') {
+						return;
+					}
+					let outputPath = path.join(this.podcastsPath, podcastName, `${user.id}-${Date.now()}.opus_string`);
 					writeStream = fs.createWriteStream(outputPath);
-					writeStream.on('finish', () =>{
-						console.log('finish');
-						this._writeStreams.delete(user.id);
-					});
-					writeStream.on('close', () =>{
-						console.log('close');
-						this._writeStreams.delete(user.id);
-					});
 					this._writeStreams.set(user.id, writeStream);
 				}
-				writeStream.write(data);
+				writeStream.write(`,${hexString}`);
 			});
-			// insert ended
 			this._voiceReceivers.set(member.voiceChannelID, voiceReceiver);
 		}).catch(console.error);
 	}
 
-	_podoff(member){
+	_podoff(member) {
 		if (!this._checkMemberHasPermissions(member)) {
 			return;
 		}
@@ -119,20 +110,19 @@ class Podbot {
 		this._voiceReceivers.delete(member.voiceChannelID);
 		this._voiceConnections.get(member.voiceChannelID).disconnect();
 		this._voiceConnections.delete(member.voiceChannelID);
-		this._podcastNames.delete(member.voiceChannelID);
 	}
 
-	_checkMemberHasPermissions(member){
+	_checkMemberHasPermissions(member) {
 		return true;
 	}
 
-	static _makePodcastsDirectory(){
+	static _makePodcastsDirectory() {
 		let dir = path.join('.', 'podcasts');
 		Podbot._makeDirectory(dir);
 		return dir;
 	}
 
-	static _makeDirectory(dir){
+	static _makeDirectory(dir) {
 		try {
 			fs.mkdirSync(dir);
 		} catch (err) {
