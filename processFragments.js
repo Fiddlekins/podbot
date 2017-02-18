07ee1c2211e93335f3237b2b7ed2d477aca22df6
Fiddlekins <fiddlekins@gmail.com>
2017-02-18 17:03:16 +0000

    Whitespace formatting and tweak to make it only use .raw_pcm files

--- processFragments.js
+++ processFragments.js
@@ -8,17 +8,17 @@ const frequency = 24000;
 
 // Define functions
 
-let extractUserId = (name) =>{
+let extractUserId = (name) => {
 	return name.split('-')[0];
 };
 
-let extractTimestamp = (name) =>{
+let extractTimestamp = (name) => {
 	return parseInt(name.split('-')[1], 10);
 };
 
-let parseTimeIntoMilliseconds = (timeString) =>{
+let parseTimeIntoMilliseconds = (timeString) => {
 	let intsDec = timeString.split('.');
-	let ints = intsDec[0].split(':').map((num) =>{
+	let ints = intsDec[0].split(':').map((num) => {
 		return parseInt(num, 10);
 	}).reverse();
 
@@ -42,7 +42,7 @@ let parseTimeIntoMilliseconds = (timeString) =>{
 		ints[i] *= 1000;
 	}
 
-	let int = ints.reduce((a, b) =>{
+	let int = ints.reduce((a, b) => {
 		return a + b;
 	}, 0);
 
@@ -54,14 +54,14 @@ let parseTimeIntoMilliseconds = (timeString) =>{
 	return int + dec;
 };
 
-let convertDurationToSamples = (duration) =>{
+let convertDurationToSamples = (duration) => {
 	return Math.ceil(frequency * duration / 1000);
 };
 
-let addDelay = (inputPath, outputPath, delay) =>{
-	return new Promise((resolve, reject) =>{
+let addDelay = (inputPath, outputPath, delay) => {
+	return new Promise((resolve, reject) => {
 		let command = `ffmpeg -f s32le -ar 24k -ac 2 -i ${inputPath} -af "adelay=${delay}|${delay}" -f s32le -acodec pcm_s32le ${outputPath}`;
-		exec(command, {}, (err, stdout, stderr) =>{
+		exec(command, {}, (err, stdout, stderr) => {
 			if (err) {
 				reject(err);
 			}
@@ -70,10 +70,10 @@ let addDelay = (inputPath, outputPath, delay) =>{
 	});
 };
 
-let addPad = (inputPath, outputPath, samples) =>{
-	return new Promise((resolve, reject) =>{
+let addPad = (inputPath, outputPath, samples) => {
+	return new Promise((resolve, reject) => {
 		let command = `ffmpeg -f s32le -ar 24k -ac 2 -i ${inputPath} -y -af apad=whole_len=${samples} -f s32le -acodec pcm_s32le ${outputPath}`;
-		exec(command, {}, (err, stdout, stderr) =>{
+		exec(command, {}, (err, stdout, stderr) => {
 			if (err) {
 				reject(err);
 			}
@@ -82,10 +82,10 @@ let addPad = (inputPath, outputPath, samples) =>{
 	});
 };
 
-let getDuration = (inputPath) =>{
-	return new Promise((resolve, reject) =>{
+let getDuration = (inputPath) => {
+	return new Promise((resolve, reject) => {
 		let command = `ffmpeg -f s32le -ar 24k -ac 2 -i ${inputPath} -f null -`;
-		exec(command, {}, (err, stdout, stderr) =>{
+		exec(command, {}, (err, stdout, stderr) => {
 			if (err) {
 				reject(err);
 			}
@@ -97,8 +97,8 @@ let getDuration = (inputPath) =>{
 	});
 };
 
-let merge = (inputPathArray, outputPath) =>{
-	return new Promise((resolve, reject) =>{
+let merge = (inputPathArray, outputPath) => {
+	return new Promise((resolve, reject) => {
 		let inputCommand = '';
 		let filterInputCommand = '';
 		let i = 0;
@@ -110,7 +110,7 @@ let merge = (inputPathArray, outputPath) =>{
 		let inputCount = inputPathArray.length;
 
 		let command = `ffmpeg ${inputCommand}-filter_complex "${filterInputCommand}amerge=inputs=${inputCount}[aout]" -map "[aout]" -ac 2 -f s32le -acodec pcm_s32le ${outputPath}`;
-		exec(command, {}, (err, stdout, stderr) =>{
+		exec(command, {}, (err, stdout, stderr) => {
 			if (err) {
 				reject(err);
 			}
@@ -119,15 +119,15 @@ let merge = (inputPathArray, outputPath) =>{
 	});
 };
 
-let concat = (inputPathArray, outputPath) =>{
-	return new Promise((resolve, reject) =>{
+let concat = (inputPathArray, outputPath) => {
+	return new Promise((resolve, reject) => {
 		let inputCommand = '';
 		for (let inputPath of inputPathArray) {
 			inputCommand += `-f s32le -ar 24k -ac 2 -i ${inputPath} `;
 		}
 
 		let command = `ffmpeg ${inputCommand}-y -filter_complex "[0:0] [1:0] concat=n=2:v=0:a=1 [a0]" -map "[a0]" -ac 2 -f s32le -acodec pcm_s32le ${outputPath}`;
-		exec(command, {}, (err, stdout, stderr) =>{
+		exec(command, {}, (err, stdout, stderr) => {
 			if (err) {
 				reject(err);
 			}
@@ -136,25 +136,29 @@ let concat = (inputPathArray, outputPath) =>{
 	});
 };
 
-let addTemporaryFile = (userId, filePath) =>{
+let addTemporaryFile = (userId, filePath) => {
 	temporaryFiles[userId] = temporaryFiles[userId] || new Set();
 	temporaryFiles[userId].add(filePath);
 };
 
-let assembleUsers = () =>{
-	fs.readdir(inputDirectory, (err, files) =>{
-		files.forEach((file) =>{
-			let userId = extractUserId(file);
-			let timestamp = extractTimestamp(file);
-			users[userId] = users[userId] || {
-					id: userId,
-					fragments: []
-				};
-			users[userId].fragments.push({
-				name: file,
-				timestamp: timestamp,
-				offset: timestamp - podcastTimestamp
-			});
+let assembleUsers = () => {
+	fs.readdir(inputDirectory, (err, files) => {
+		files.forEach((file) => {
+			let ext = path.extname(file);
+			if (ext === '.raw_pcm') {
+				let filename = path.basename(file, ext);
+				let userId = extractUserId(filename);
+				let timestamp = extractTimestamp(filename);
+				users[userId] = users[userId] || {
+						id: userId,
+						fragments: []
+					};
+				users[userId].fragments.push({
+					name: filename,
+					timestamp: timestamp,
+					offset: timestamp - podcastTimestamp
+				});
+			}
 		});
 
 		for (let userId in users) {
@@ -167,8 +171,8 @@ let assembleUsers = () =>{
 	});
 };
 
-let delayFirstFragment = (user) =>{
-	user.fragments.sort((a, b) =>{
+let delayFirstFragment = (user) => {
+	user.fragments.sort((a, b) => {
 		return a.offset - b.offset;
 	});
 	let fragment = user.fragments[0];
@@ -176,13 +180,13 @@ let delayFirstFragment = (user) =>{
 	user.flipFlop = 1;
 	let outputPath = path.join(inputDirectory, `${user.id}_${user.flipFlop}`);
 	let delay = fragment.offset;
-	addDelay(inputPath, outputPath, delay).then((stdout, stderr) =>{
+	addDelay(inputPath, outputPath, delay).then((stdout, stderr) => {
 		user.fragments.shift();
 		padAndConcatenateFragments(user);
 	}).catch(console.error);
 };
 
-let padAndConcatenateFragments = (user) =>{
+let padAndConcatenateFragments = (user) => {
 	let nextFragment = user.fragments.shift();
 	if (nextFragment) {
 		let totalSamples = convertDurationToSamples(nextFragment.offset);
@@ -191,37 +195,37 @@ let padAndConcatenateFragments = (user) =>{
 		addTemporaryFile(user.id, inputPath);
 		addTemporaryFile(user.id, outputPath);
 		user.flipFlop = 1 - user.flipFlop;
-		addPad(inputPath, outputPath, totalSamples).then((stdout, stderr) =>{
+		addPad(inputPath, outputPath, totalSamples).then((stdout, stderr) => {
 			inputPath = path.join(inputDirectory, `${user.id}_${user.flipFlop}`);
 			outputPath = path.join(inputDirectory, `${user.id}_${1 - user.flipFlop}`);
 			user.flipFlop = 1 - user.flipFlop;
 			let inputPathArray = [inputPath, path.join(inputDirectory, nextFragment.name)];
-			concat(inputPathArray, outputPath).then((stderr, stdout) =>{
+			concat(inputPathArray, outputPath).then((stderr, stdout) => {
 				console.log(`User ${user.id} has completed ${100 * (user.fragmentCount - user.fragments.length) / user.fragmentCount}%`);
 				if (user.fragments.length) {
 					padAndConcatenateFragments(user);
 				} else {
-					fs.rename(outputPath, path.join(inputDirectory, user.id), (err) =>{
+					fs.rename(outputPath, path.join(inputDirectory, user.id), (err) => {
 						cleanTemporaryFiles(user);
 					});
 				}
 			}).catch(console.error);
 		}).catch(console.error);
 	} else {
-		fs.rename(path.join(inputDirectory, `${user.id}_${user.flipFlop}`), path.join(inputDirectory, user.id), (err) =>{
+		fs.rename(path.join(inputDirectory, `${user.id}_${user.flipFlop}`), path.join(inputDirectory, user.id), (err) => {
 			cleanTemporaryFiles(user);
 		});
 	}
 };
 
-let addFragmentDelay = (user) =>{
+let addFragmentDelay = (user) => {
 	let outstandingFragmentCount = user.fragments.length;
 	for (let fragment of user.fragments) {
 		let inputPath = path.join(inputDirectory, fragment.name);
 		let outputPath = path.join(inputDirectory, fragment.name + '-delay');
 		addTemporaryFile(user.id, outputPath);
 		let delay = fragment.offset;
-		addDelay(inputPath, outputPath, delay).then((stdout, stderr) =>{
+		addDelay(inputPath, outputPath, delay).then((stdout, stderr) => {
 			outstandingFragmentCount--;
 			if (outstandingFragmentCount <= 0) {
 				addFragmentDelayFinished(user);
@@ -230,24 +234,24 @@ let addFragmentDelay = (user) =>{
 	}
 };
 
-let addFragmentDelayFinished = (user) =>{
-	user.fragments.sort((a, b) =>{
+let addFragmentDelayFinished = (user) => {
+	user.fragments.sort((a, b) => {
 		return b.offset - a.offset;
 	});
 	let inputPath = path.join(inputDirectory, user.fragments[0].name + '-delay');
-	getDuration(inputPath).then((duration, stdout, stderr) =>{
+	getDuration(inputPath).then((duration, stdout, stderr) => {
 		let samples = convertDurationToSamples(duration + 1000); // Add a second to ensure all fragments hit same total duration
 		addFragmentPad(user, samples);
 	}).catch(console.error);
 };
 
-let addFragmentPad = (user, samples) =>{
+let addFragmentPad = (user, samples) => {
 	let outstandingFragmentCount = user.fragments.length;
 	for (let fragment of user.fragments) {
 		let inputPath = path.join(inputDirectory, fragment.name + '-delay');
 		let outputPath = path.join(inputDirectory, fragment.name + '-delay-pad');
 		addTemporaryFile(user.id, outputPath);
-		addPad(inputPath, outputPath, samples).then((stdout, stderr) =>{
+		addPad(inputPath, outputPath, samples).then((stdout, stderr) => {
 			outstandingFragmentCount--;
 			if (outstandingFragmentCount <= 0) {
 				addFragmentPadFinished(user);
@@ -256,22 +260,22 @@ let addFragmentPad = (user, samples) =>{
 	}
 };
 
-let addFragmentPadFinished = (user) =>{
-	let inputPathArray = user.fragments.map((fragment) =>{
+let addFragmentPadFinished = (user) => {
+	let inputPathArray = user.fragments.map((fragment) => {
 		return path.join(inputDirectory, fragment.name + '-delay-pad');
 	});
 	let outputPath = path.join(inputDirectory, user.id);
-	merge(inputPathArray, outputPath).then((stdout, stderr) =>{
+	merge(inputPathArray, outputPath).then((stdout, stderr) => {
 		console.log(stdout);
 		cleanTemporaryFiles(user);
 	}).catch(console.error);
 };
 
-let cleanTemporaryFiles = (user) =>{
+let cleanTemporaryFiles = (user) => {
 	let files = temporaryFiles[user.id];
 	if (files) {
-		files.forEach((filePath) =>{
-			fs.unlink(filePath, (err) =>{
+		files.forEach((filePath) => {
+			fs.unlink(filePath, (err) => {
 				files.delete(filePath);
 				if (files.size <= 0) {
 					cleanTemporaryFilesFinished(user);
@@ -283,7 +287,7 @@ let cleanTemporaryFiles = (user) =>{
 	}
 };
 
-let cleanTemporaryFilesFinished = (user) =>{
+let cleanTemporaryFilesFinished = (user) => {
 	console.log(`User ${user.id} finished!`);
 };
 
