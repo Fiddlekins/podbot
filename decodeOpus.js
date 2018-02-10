b754f6cb4257e6303a716c9ca83dc118128d0821
Fiddlekins <fiddlekins@gmail.com>
2018-02-10 20:26:42 +0000

    Refactored code a bit

--- decodeOpus.js
+++ decodeOpus.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 const opus = require('node-opus');
 
@@ -8,10 +8,9 @@ const rate = 48000;
 const frame_size = 1920;
 const channels = 2;
 
-let total = 0;
-let complete = 0;
+const INPUT_EXTENSION = '.opus_string';
 
-let getDecodedFrame = (frameString, encoder, filename) => {
+function getDecodedFrame(frameString, encoder, filename) {
 	let buffer = Buffer.from(frameString, 'hex');
 	try {
 		buffer = encoder.decode(buffer, frame_size);
@@ -24,54 +23,59 @@ let getDecodedFrame = (frameString, encoder, filename) => {
 		}
 	}
 	return buffer;
-};
+}
 
-let convertOpusStringToRawPCM = (inputPath, filename) => {
-	total++;
-	let encoder = new opus.OpusEncoder(rate, channels);
-	const inputStream = fs.createReadStream(inputPath);
-	const outputStream = fs.createWriteStream(path.join(path.dirname(inputPath), `${filename}.raw_pcm`));
-	let data = '';
-	inputStream.on('data', chunk => {
-		data += chunk.toString();
-		const frames = data.split(',');
-		if (frames.length) {
-			data = frames.pop();
-		}
-		for (let frame of frames) {
-			if (frame !== '') {
-				const decodedBuffer = getDecodedFrame(frame, encoder, filename);
-				if (decodedBuffer) {
-					outputStream.write(decodedBuffer);
+function convertOpusStringToRawPCM(inputPath, filename) {
+	return new Promise((resolve, reject) => {
+		let encoder = new opus.OpusEncoder(rate, channels);
+		const inputStream = fs.createReadStream(inputPath);
+		const outputStream = fs.createWriteStream(path.join(path.dirname(inputPath), `${filename}.raw_pcm`));
+		let data = '';
+		inputStream.on('data', chunk => {
+			data += chunk.toString();
+			const frames = data.split(',');
+			if (frames.length) {
+				data = frames.pop();
+			}
+			for (let frame of frames) {
+				if (frame !== '') {
+					const decodedBuffer = getDecodedFrame(frame, encoder, filename);
+					if (decodedBuffer) {
+						outputStream.write(decodedBuffer);
+					}
 				}
 			}
-		}
-	});
-	inputStream.on('end', () => {
-		outputStream.end((err) => {
-			if (err) {
-				console.error(err);
-			}
-			complete++;
-			console.log(`Completed ${100 * complete / total}%`);
+		});
+		inputStream.on('end', () => {
+			outputStream.end((err) => {
+				if (err) {
+					console.error(err);
+					reject(err);
+				}
+				resolve();
+			});
 		});
 	});
-};
+}
 
-let convertAllOpusStringToRawPCM = (inputDirectory) => {
-	fs.readdir(inputDirectory, (err, files) => {
-		if (err) {
-			console.error(`Could not read input due to: ${err}`);
-		} else {
-			files.forEach((file) => {
-				let ext = path.extname(file);
-				if (ext === '.opus_string') {
-					convertOpusStringToRawPCM(path.join(inputDirectory, file), path.basename(file, ext));
-				}
-			});
-		}
-	});
-};
+async function convertAllOpusStringToRawPCM(inputDirectory) {
+	let files;
+	try {
+		files = await fs.readdir(inputDirectory);
+	} catch (err) {
+		console.error(`Could not read input due to: ${err}`);
+	}
+	files = files.filter(file => path.extname(file) === INPUT_EXTENSION);
+	let totalCount = files.length;
+	let completedCount = 0;
+	const updateProgress = () => {
+		completedCount++;
+		console.log(`Completed ${100 * completedCount / totalCount}%`);
+	};
+	await Promise.all(files.map(file => {
+		return convertOpusStringToRawPCM(path.join(inputDirectory, file), path.basename(file, INPUT_EXTENSION)).then(updateProgress);
+	}));
+}
 
 let inputDirectory = path.join('podcasts', process.argv[2]);
 
