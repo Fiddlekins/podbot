015da67c825ac874043051d98efd56e7a4053463
Joey Wang <joeyw@Joeys-M1-Pro.local>
2021-04-25 00:11:19 +0100

    must with 0.0.0.0

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ var port =  process.env.PORT || 8080;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/json'});
   res.end('{"status":"OK"}');
-}).listen(port);
+}).listen(port, '0.0.0.0');
 
 init();
 
